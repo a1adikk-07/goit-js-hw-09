7f391c1dafb9b393391cc3a8c10b2568051e2dcf
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2023-11-13 23:57:58 +0200

    hw9 js task one finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    colorStart: document.querySelector('[data-start]'),
+    colorStop: document.querySelector('[data-stop]'),
+};
+const body = document.querySelector('body');
 
+refs.colorStart.addEventListener('click', handleClickStartBtn);
+refs.colorStop.addEventListener('click', handleClickStopBtn);
+
+refs.colorStop.setAttribute('disabled', true)
+
+function handleClickStartBtn() {
+    refs.colorStart.setAttribute('disabled', true);
+    id = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.colorStop.removeAttribute('disabled');
+}
+
+function handleClickStopBtn() {
+   clearInterval(id);
+   refs.colorStop.setAttribute('disabled', true)
+   refs.colorStart.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+}
